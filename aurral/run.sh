7c0c88685f79cd9ee77fb85069b4ccd9ec88447d
olliedown <51142696+olliedown@users.noreply.github.com>
2026-03-31 20:36:08 +0100

    Update run.sh

--- aurral/run.sh
+++ aurral/run.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Map Config Folder"
-ln -sf /app/backend /config
-ln -sf /app/backend /data
+echo "ln -s /app/backend /config"
+ln -s /app/backend /config
 
 echo "Starting"
 exec node /app/backend/server.js
