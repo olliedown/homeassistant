51cf6fbff5be21bd273df8c44d93f3c3651584af
olliedown <51142696+olliedown@users.noreply.github.com>
2026-03-31 20:40:19 +0100

    Update run.sh

--- aurral/run.sh
+++ aurral/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-echo "ln -s /app/backend /config"
-ln -s /app/backend /config
+#echo "ln -s /app/backend /config"
+#ln -s /app/backend /config
 
 echo "Starting"
 exec node /app/backend/server.js
